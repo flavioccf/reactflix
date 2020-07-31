c994498c89f4832fa477b6c79d0f82321ff6e58c
Flávio Carvalho Filho <flavioccf@hotmail.com>
2020-07-30 23:19:35 -0300

    add server side

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
+require('dotenv').config();
 
 ReactDOM.render(
   <BrowserRouter>
-  <Switch>
-    <Route path="/" component={Home} exact></Route>
-    <Route path="/cadastro/video" component={CadastroVideo} exact></Route>
-    <Route path="/cadastro/categoria" component={CadastroCategoria} exact></Route>
-    <Route component={() => (<div>404</div>)}></Route>
-  </Switch>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route component={() => (<div>404</div>)} />
+    </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,84 +1,86 @@
-import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    const initialValues = {
-        name: '',
-        description: '',
-        color: '',
-    }
-    const [categories, setCategories] = useState([]);
-    const [values, setValues] = useState(initialValues);
+  const initialValues = {
+    name: '',
+    description: '',
+    color: '',
+  };
+  const [categories, setCategories] = useState([]);
+  const [values, setValues] = useState(initialValues);
 
-    function setValue(key, val) {
-        setValues({
-            ...values,
-            [key]: val,
-        })
-    }
-    function handleChange(e) {
-        const {getAttribute, value} = e.target;
-        setValue(getAttribute, value);
-    }
-    return(
-            <PageDefault>
-                <h1>Cadastro de Categoria: {values.name}</h1>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    setCategories([
-                        ...categories,
-                        values
-                    ]);
-                    setValues(initialValues);
-                }}> 
-                    <FormField
-                        label="Nome da categoria"
-                        type="text"
-                        name="name"
-                        value={values.nome}
-                        onChange={handleChange}
-                    ></FormField>
-                    <div>
-                    <label>Descriçao:</label>
-                    <textarea
-                    required
-                    name="description"
-                    value={values.description}
-                    onChange={
-                        handleChange
-                    }
-                    ></textarea>
-                    </div>
-                    <FormField
-                        label="Descrição"
-                        type="textarea"
-                        name="description"
-                        value={values.description}
-                        onChange={handleChange}
-                    ></FormField>
-                    <FormField
-                        label="Cor"
-                        type="color"
-                        name="color"
-                        value={values.nome}
-                        onChange={handleChange}
-                    ></FormField>
-                    <button>Salvar</button>
-                </form>
-                <ul>
-                    {categories.map((categoria, index) => {
-                        return (
-                        <li key={`${categoria}${index}`}>{ categoria.name }</li>
-                        )
-                    })}
-                </ul>
-                <Link to="/">
-                    Ir para Home
-                </Link>
-            </PageDefault>
-    );
+  function setValue(key, val) {
+    setValues({
+      ...values,
+      [key]: val,
+    });
+  }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setValue(name, value);
+  }
+
+  useEffect(() => {
+    const URL = process.env.REACT_APP_CATEGORIES;
+    fetch(URL).then(async (response) => {
+      const responseBody = await response.json();
+      setCategories([
+        ...responseBody,
+      ]);
+    });
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.name}
+      </h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setCategories([
+          ...categories,
+          values,
+        ]);
+        setValues(initialValues);
+      }}
+      >
+        <FormField
+          label="Nome da categoria"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor"
+          type="color"
+          name="color"
+          value={values.color}
+          onChange={handleChange}
+        />
+        <Button>Salvar</Button>
+      </form>
+      <ul>
+        {categories.map((categoria, index) => (
+          <li key={`${categoria.name}${index}`}>{ categoria.name }</li>
+        ))}
+      </ul>
+      <Link to="/">
+        Ir para Home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
